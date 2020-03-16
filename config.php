88cdc846dd6c3ab756e182b54412d36ced8f93d5
c011452576 <c011452576@edu.teu.ac.jp>
2020-03-16 14:20:06 +0900

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors', 1);
+
+
+define('DSN', 'mysql:host=localhost;dbname='); // Enter your setting dbname
+define('DB_USERNAME', ''); // Enter your setting username
+define('DB_PASSWORD', ''); // Enter your setting password
+
+?>
